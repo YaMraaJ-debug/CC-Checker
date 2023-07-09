a2c7981e3e26ba6c6fffab7ff6d0f56ed4febd31
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-07-09 01:38:24 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# Telegram API configuration
+API_ID = "YOUR_API_ID"
+API_HASH = "YOUR_API_HASH"
+PHONE_NUMBER = "YOUR_PHONE_NUMBER"
+
+# Bot configuration
+CHANNEL = "https://t.me/your_channel"  # The channel URL or username
+LOG_CHANNEL = "@your_log_channel"  # The log channel username or ID
+BOT_USERNAME = "@your_bot_username"  # Your bot's username
+
+# MongoDB configuration
+MONGO_URI = "mongodb://localhost:27017"  # Your MongoDB connection URI
+DATABASE_NAME = "telegram_bot"  # The name of the MongoDB database
+COLLECTION_NAME = "bot_data"  # The name of the MongoDB collection
+
+# Telegraph configuration
+TELEGRAPH_TOKEN = "YOUR_TELEGRAPH_TOKEN"  # Your Telegraph token obtained from the Telegraph API platform
